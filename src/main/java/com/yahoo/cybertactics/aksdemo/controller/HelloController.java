e8981f258005735a0daa688310f17961a6d99864
syril pagunsan <syril.pagunsan@godigitalcorp.com>
2022-02-08 16:28:49 +0800

    changed the text

--- src/main/java/com/yahoo/cybertactics/aksdemo/controller/HelloController.java
+++ src/main/java/com/yahoo/cybertactics/aksdemo/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-       return "this is a test";
+       return "Hello World";
    }
 }
