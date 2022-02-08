d2dfa0352540ce41cf734292130044e4f85de388
syril pagunsan <syril.pagunsan@godigitalcorp.com>
2022-02-08 10:35:30 +0800

    this is a test

--- src/main/java/com/yahoo/cybertactics/aksdemo/controller/HelloController.java
+++ src/main/java/com/yahoo/cybertactics/aksdemo/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-       return "Hello World updated this part";
+       return "Hello World updated this hello guys";
    }
 }
